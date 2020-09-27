ce293a3d92aea99cfa084ba0fc96852d63d5a4af
Manh Bui <buimanh17112001@gmail.com>
2020-09-27 11:53:50 +0700

    remove css file

--- src/index.js
+++ src/index.js
@@ -6,15 +6,21 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3001;
 
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(morgan('combined'));
 //template engine   
-app.engine('handlebars', handlebars());
-app.set('view engine', 'handlebars');
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'assets/views'));
 
 app.get('/', (req, res) => {
-    return res.send('home');
+    return res.render('home');
 });
+app.get('/route_1', (req, res) => {
+    return res.render('route_1');
+})
 
 app.listen(port, () => {
     console.log(`app is listening port ${port}`);
